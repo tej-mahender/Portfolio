c8c502cc2b61c1c32591e5717d3236bc02c3c05b
tej <reddytej15@gmail.com>
2024-11-23 09:58:46 +0530

    design: Landing Page

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+    <header>
+      <div className="header-container">
+        <div className="content">
+          <h1 className="logo" aria-level="1">
+            Hello, I'm 
+          </h1>
+          <h2 className="main-title" aria-level="2">
+            Tej Mahendra
+          </h2>
+          <p className="subtitle">
+            A passionate software developer
+          </p>
+          </div>
+          <button className="scroll-button">
+            <span className="arrow">↓</span> Scroll
+          </button>
+      </div>
+    </header>
+    <body>
+      
+    </body>
+    </>
+  );
+};
+
+export default App;
